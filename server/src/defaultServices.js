2d756eedfac4b82f4e5bf42d18f9fa2314bc38da
Yunusbasha Shaik <149937279+Yunusbashashaik@users.noreply.github.com>
2026-09-05 18:09:08 +0000

    Update defaultServices.js

--- server/src/defaultServices.js
+++ server/src/defaultServices.js
@@ -1,1 +1,1 @@
-export { DEFAULT_SERVICES, STANDARD_DESCRIPTION_AR, STANDARD_DESCRIPTION_EN } from "../../shared/defaultServices.js";
+export { DEFAULT_SERVICES } from "../../shared/defaultServices.js";
